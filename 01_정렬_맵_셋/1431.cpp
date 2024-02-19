01b156fc37b98dfaf55c66c0281f03a5ff21b5c2
dabiih <158825877+dabiih@users.noreply.github.com>
2024-02-20 07:34:13 +0900

    Update 1431.cpp

---
+++ 01_정렬_맵_셋/1431.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+using namespace std;
+
+int cmp(string a, string b) {
+    int asize = a.size();
+    int bsize = b.size();
+    
+    if (asize != bsize) {
+        return asize < bsize;
+    }
+
+    int asum = 0, bsum = 0;
+    for (int i = 0; i < asize; i++) {
+        if (a[i] >= '0' && a[i] <= '9') {
+            asum += a[i] - '0';
+        }
+        if (b[i] >= '0' && b[i] <= '9') {
+            bsum += b[i] - '0';
+        }
+    }
+
+    if (asum != bsum) {
+        return asum < bsum;
+    }
+
+    return a < b;
+}
+
+int main() {
+    int N;
+    cin >> N;
+
+    vector<string> serial(N);
+    for (int i = 0; i < N; i++) {
+        cin >> serial[i];
+    }
+
+    sort(serial.begin(), serial.end(), cmp);
+
+    for (int i = 0; i < N; i++) {
+        cout << serial[i] << "\n";
+    }
+
+    return 0;
+}
